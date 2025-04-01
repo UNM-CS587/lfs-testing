cd95e4b0df257b8850a829049aef9859b7203b95
Patrick Bridges <patrickb314@gmail.com>
2025-04-01 14:12:23 -0600

    Added additional write test case

--- test_write.py
+++ test_write.py
@@ -10,8 +10,10 @@ def test_single_write():
     # Use LFS_Log to create a new journal
     l = LFS_Log("lfstest.log")
 
-    # Create an empty file in the root directory
-    fnum = l.creat(0, LFS_REGULAR_FILE, "testfile.bin")
+    # Create an empty file in the root directory and look it up
+    l.creat(0, LFS_REGULAR_FILE, "testfile.bin")
+    fnum = l.lookup(0, "testfile.bin")
+    assert fnum > 0
 
     # Stat the file and check its length
     type, size = l.stat(fnum)
@@ -20,9 +22,9 @@ def test_single_write():
 
     # Create a 4k byte array to write to the file
     b = bytearray(4096)
-
     for i in range(4096):
         b[i] = (4096 - i) & 0xff
+
     # Write it to the file
     l.write(fnum, 0, b)
 
@@ -33,6 +35,34 @@ def test_single_write():
 
     # Close the log
     l = None
+
+def test_invalid_write():
+    # Use LFS_Log to open the existing log
+    l = LFS_Log("lfstest.log")
+
+    fnum = l.lookup(0, "testfile.bin")
+    assert fnum > 0
+    
+    # Create a 4k byte array to write to the file
+    b = bytearray(4096)
+    for i in range(4096):
+        b[i] = (4096 - i) & 0xff
+
+    # Write to invalid inode number
+    try:
+        l.write(514, 0, b)
+    except:
+        pass
+    else:
+        throw AssertError("Write to invalid inode number did not throw an exception")
+
+    # Write to invalid block number
+    try:
+        l.write(fnum, 15, b)
+    except:
+        pass
+    else:
+        throw AssertError("Write to invalid inode number did not throw an exception")
 
 def test_single_read():
     # Use LFS_Log to open the existing log
@@ -114,7 +144,7 @@ def test_skip_read():
     # Block 2 hasn't been written but should still be in the file
     # since its inside the length. Read should return 4096 bytes of 0
     b = l.read(fnum, 2, b)
-    assert len(b) = 4096
+    assert len(b) == 4096
     for i in range(4096):
         assert b[i] == 0 
 
